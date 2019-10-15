e5364cdf009c71ec69e504d3290982709be8fbc2
LyNoogie <khanhly.t.nguyen@gmail.com>
2019-10-15 13:08:11 -0700

    cursor looks clickable

--- WebApplication1/Migrations/LearningOutcomes/20191014231748_users.Designer.cs
+++ WebApplication1/Migrations/LearningOutcomes/20191014231748_users.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication1.Models;
 
 namespace WebApplication1.Migrations.LearningOutcomes
 {
     [DbContext(typeof(LearningOutcomesContext))]
-    partial class LearningOutcomesContextModelSnapshot : ModelSnapshot
+    [Migration("20191014231748_users")]
+    partial class users
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
